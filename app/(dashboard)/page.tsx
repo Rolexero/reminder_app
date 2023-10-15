3a42b718b31eae3ce0705408b5e783be4d771842
Rolexero <rotimiolaolu@gmail.com>
2023-10-15 21:17:00 +0100

    feat: :sparkles: work on the design, setup and auth implementation

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,7 @@
+import CreateCollectionBtn from "@/components/CreateCollectionBtn";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
+import { prisma } from "@/lib/prismadb";
 import { currentUser } from "@clerk/nextjs";
 import Image from "next/image";
 import { redirect } from "next/navigation";
@@ -11,9 +14,10 @@ export default function Home() {
         {/* @ts-expect-error Server Component */}
         <WelcomeMsg />
       </Suspense>
-      {/* <Suspense fallback={<div>Loading collections...</div>}> */}
-      {/* <CollectionList /> */}
-      {/* </Suspense> */}
+      <Suspense fallback={<div>Loading collections...</div>}>
+        {/* @ts-expect-error Server Component */}
+        <CollectionList />
+      </Suspense>
     </>
   );
 }
@@ -43,4 +47,40 @@ function WelcomeMsgFallback() {
       </h1>
     </div>
   );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+  console.log(collections);
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          {/* <SadFace /> */}
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
+
+  // return (
+  //   <>
+  //     {/* <CreateCollectionBtn /> */}
+  //     <div className="flex flex-col gap-4 mt-6">
+  //       {collections.map((collection) => (
+  //         <CollectionCard key={collection.id} collection={collection} />
+  //       ))}
+  //     </div>
+  //   </>
+  // );
 }
---
+++ components/CreateCollectionBtn.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+// import CreateCollectionSheet from "./CreateCollectionSheet";
+
+function CreateCollectionBtn() {
+  const [open, setOpen] = useState(false);
+  const handleOpenChange = (open: boolean) => setOpen(open);
+
+  return (
+    <div
+      className="
+    w-full rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[1px]"
+    >
+      <Button
+        variant={"outline"}
+        className="dark:text-white w-full dark:bg-neutral-950 bg-white"
+        onClick={() => setOpen(true)}
+      >
+        <span className="bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent">
+          Create collection
+        </span>
+      </Button>
+      {/* <CreateCollectionSheet open={open} onOpenChange={handleOpenChange} /> */}
+    </div>
+  );
+}
+
+export default CreateCollectionBtn;
